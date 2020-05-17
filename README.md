35ca9f16df9544ba7daaf211bd64aedd398732b2
wuqingze <nongxiaolang@gmail.com>
2020-05-17 22:21:40 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # fastermaven
 一个比maven更快的编译运行工具
+一个很常见的场景是我们修改了某个Java文件，需要编译运行验证一下结果，
+常见的用法是用mvn test命令，会将修改的文件编译，运行单元测试。
+这个工具就是将上面的过程提取出来，进行定制化加速。
+
+** 用法
+在maven工程第一级目录下（和src同一目录）运行t.sh脚本，会产生一下4个脚本，
+compile_main.sh--编译main目录的文件
+run_main.sh--运行main目录的main函数
+compile_test.sh--编译test目录l下的修改文件
+run_test.sh--运行单元测试
---
+++ t.sh
@@ -0,0 +1,12 @@
+v=~/.m2/repository/
+mvn dependency:resolve | grep :jar | awk '{split($2,a,":"); gsub("\\.","/",a[1]);print a[1],a[2],a[4]}' | awk '{print $1"/"$2"/"$3"/"$2"-"$3".jar"}' | awk -v p="$v" '{print p$1}' > tt
+v1=$(sed -n 1p tt)
+v2=$(sed -n 2p tt)
+v3=$(sed -n 3p tt)
+v4=$(sed -n 4p tt)
+cp=$v1:$v2:$v3:$v4
+echo "javac -d ./target/classes/ -cp ./target/classes:"$cp" ./src/main/java/.java" > compile_main.sh
+echo "java -cp ./target/classes:"$cp" ./target/classes/" > run_main.sh
+echo "javac -d ./target/test-classes/ -cp ./target/test-classes:./target/classes:"$cp" ./src/test/java/.java" > compile_test.sh 
+echo "java -cp ./target/test-classes:./target/classes:"$cp" ./target/test-classes/" > run_test.sh
+rm tt
